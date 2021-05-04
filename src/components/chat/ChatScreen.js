d8e90b5b6d86b69082fcb2cab7acb81d14bfe3af
Z3hcans <izan.perezsanchez@gmail.com>
2021-05-04 11:53:46 +0200

    chat styles

--- src/components/chat/ChatScreen.js
+++ src/components/chat/ChatScreen.js
@@ -8,20 +8,64 @@ export const ChatScreen = () => {
       type: "message",
       user: {
         name: "asdasda",
-        photoUrl:
+        photo:
+          "https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+      },
+    },
+    {
+      content: "asdasd",
+      type: "message",
+      user: {
+        name: "asdasda",
+        photo:
+          "https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+      },
+    },
+    {
+      content: "asdasd",
+      type: "message",
+      user: {
+        name: "asdasda",
+        photo:
+          "https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+      },
+    },
+    {
+      content: "asdasd",
+      type: "message",
+      user: {
+        name: "asdasda",
+        photo:
+          "https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
+      },
+    },
+    {
+      content: "asdasd",
+      type: "message",
+      user: {
+        name: "asdasda",
+        photo:
           "https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg",
       },
     },
   ];
 
   return (
-    <ul className="chat">
-      {comments.map((comment) => (
-        <ComentChat
-          {...comment}
-          key={comment.user.name + new Date().getMilliseconds}
-        />
-      ))}
-    </ul>
+      <div className="chatBox">
+          <ul className="chat">
+            {comments.map((comment) => (
+              <ComentChat
+                {...comment}
+                key={comment.user.name + new Date().getMilliseconds}
+              />
+            ))}
+          </ul>
+        <div className="chatInputBox">
+          <input type="text" className="chatInput form-control" placeholder="Type your message for the chat"/>
+          <div className="sendButton">
+          <i className="fas fa-location-arrow"></i>
+          </div>
+        </div>
+      </div>
   );
 };
--- src/components/chat/ComentChat.js
+++ src/components/chat/ComentChat.js
@@ -4,6 +4,7 @@ export const ComentChat = ({ content, type, user }) => {
   console.log(content, type, user);
   return (
     <li className="comment">
+      <img src={user.photo} className="userChatPhoto" alt="userPhoto" />
       <span className="userName"> {user.name}</span>: {content}
     </li>
   );
